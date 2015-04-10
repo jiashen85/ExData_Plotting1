4faf4593bd1f983fd920609321fcc372e53f9916
Tracy <jia.t.shen@gmail.com>
2015-04-09 23:34:33 -0400

    first commit

---
+++ plot1.R
@@ -0,0 +1,8 @@
+STEP1: Read 1/2/2007 and 2/2/2007 into R
+STEP2: convert date and time to Date/Time classes using strptime(),as.Date()
+STEP3:contruct plots usign base plotting system
+  -fucntions will be used: hist(), 1/2 Thu,2/2 Fri
+  -legend(),col, mfrow, 
+
+
+#Step1: Read two days' data into R
