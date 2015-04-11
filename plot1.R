cec85c47d44f5b78e09003cbbedc8c2166db9b90
Tracy <jia.t.shen@gmail.com>
2015-04-11 16:39:43 -0400

    finished

--- plot1.R
+++ plot1.R
@@ -1,8 +1,12 @@
-STEP1: Read 1/2/2007 and 2/2/2007 into R
-STEP2: convert date and time to Date/Time classes using strptime(),as.Date()
-STEP3:contruct plots usign base plotting system
-  -fucntions will be used: hist(), 1/2 Thu,2/2 Fri
-  -legend(),col, mfrow, 
+#Step1: Read the data of 1/2/2007 and 2/2/2007 into R
 
+df<-read.table("./household_power_consumption.txt",skip=66637,nrows=2880,sep=";",na.strings="?")
+#reassign column names as the selective reading didn't read in the column names.
+names(df)<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+ 
+#make another column put Date and Time together which are already converted using strptime function. 
+df$Datetime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+ 
 
-#Step1: Read two days' data into R
+#plot 1: histagram of Global Active Power's frequency 
+hist(df$Global_active_power, main = "Global Active Power", ylab = "Frequency",xlab = "Global Active Power (kilowatts)", col = "red", breaks = 13, ylim = c(0,1200), xlim = c(0, 6), xaxp = c(0, 6, 3))
